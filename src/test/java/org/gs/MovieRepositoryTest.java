1bb03b3a431fe867ea3d71b585205add88a594aa
C1332335 Fernando Martins Ferreira <c1332335@interno.bb.com.br>
2024-08-20 18:10:34 -0300

    Implementando testes unitários de 'MovieRepository'

--- src/test/java/org/gs/MovieRepositoryTest.java
+++ src/test/java/org/gs/MovieRepositoryTest.java
@@ -1,11 +1,16 @@
 package org.gs;
 
-import static org.junit.jupiter.api.Assertions.*;
-
 import io.quarkus.test.junit.QuarkusTest;
-import java.util.List;
-import javax.inject.Inject;
+import org.gs.factory.MovieFactory;
 import org.junit.jupiter.api.Test;
+
+import javax.inject.Inject;
+import java.util.List;
+import java.util.Objects;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @QuarkusTest
 class MovieRepositoryTest {
@@ -13,9 +18,25 @@ class MovieRepositoryTest {
   @Inject
   MovieRepository movieRepository;
 
-  @Test
-  void findByCountryOK() {}
+  @Inject
+  MovieFactory movieFactory;
 
   @Test
-  void findByCountryKO() {}
+  void findByCountryOK() {
+    Movie movie = movieFactory.createAndPersist();
+    List<Movie> result = movieRepository.findByCountry(movie.getCountry());
+
+    assertNotNull(result);
+    assertEquals(1, result.size());
+    assertTrue(result.stream().anyMatch(m -> Objects.equals(movie.getId(), m.getId())));
+  }
+
+  @Test
+  void findByCountryKO() {
+    Movie movie = movieFactory.createAndPersist();
+    List<Movie> result = movieRepository.findByCountry(movie.getCountry() + " invalid");
+
+    assertNotNull(result);
+    assertTrue(result.isEmpty());
+  }
 }
